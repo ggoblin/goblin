ff8ed77298887712c969ca266df4f6978c4319f3
eternnoir <eternnoir@gmail.com>
2016-03-05 00:05:41 +0800

    Add template support.

--- goblin/goblin.go
+++ goblin/goblin.go
@@ -3,9 +3,19 @@ package goblin
 import (
 	"github.com/labstack/echo"
 	mw "github.com/labstack/echo/middleware"
+	"html/template"
+	"io"
 	"net/http"
 	"strconv"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 type Goblin struct {
 	server *echo.Echo
@@ -23,8 +33,19 @@ func (gb *Goblin) StartServer() {
 	// Middleware
 	gb.server.Use(mw.Logger())
 	gb.server.Use(mw.Recover())
-	gb.server.Get("/", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+
+	gb.server.Static("/static", "goblin/data/static")
+	gb.SetupHandler()
 	gb.server.Run(":" + strconv.Itoa(gb.config.Port))
+}
+
+func (gb *Goblin) SetupHandler() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("goblin/data/templates/*.html")),
+	}
+	gb.server.SetRenderer(t)
+
+	gb.server.Get("/", func(c *echo.Context) error {
+		return c.Render(http.StatusOK, "index", "")
+	})
 }
