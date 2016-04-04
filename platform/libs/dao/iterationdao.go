a14c2bde263f3a7c330dea56f34ac3460f0b3750
eternnoir <eternnoir@gmail.com>
2016-04-04 12:19:48 +0800

    Add Get all iterations method.

--- platform/libs/dao/iterationdao.go
+++ platform/libs/dao/iterationdao.go
@@ -1,1 +1,22 @@
 package dao
+
+import (
+	_ "database/sql"
+	log "github.com/Sirupsen/logrus"
+	"github.com/ggoblin/goblin/platform/libs/model"
+	"github.com/ggoblin/goblin/platform/libs/utils"
+	_ "github.com/lib/pq"
+)
+
+func GetAllIterations() ([]model.Iteration, error) {
+	db, err := utils.GetDefaultDb()
+	log.Debug("Start Get all iterations.")
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	var iterations []model.Iteration
+	db.Find(&iterations)
+	log.Debug("Start Get all iterations.")
+	return iterations, nil
+}
