d5a7bb9655a2a5fa7d54d054e93a07c03f294b66
eternnoir <eternnoir@gmail.com>
2016-04-05 19:50:57 +0800

    Add gorm logging support.

--- platform/libs/utils/dbchelper.go
+++ platform/libs/utils/dbchelper.go
@@ -15,7 +15,13 @@ func GetDefaultDb() (*gorm.DB, error) {
 	if Dbc == "" {
 		return nil, fmt.Errorf("Dbconnection string is empty.")
 	}
-	return gorm.Open(Dbtype, Dbc)
+	dbc, err := gorm.Open(Dbtype, Dbc)
+	if err != nil {
+		return nil, err
+	}
+	dbc.SetLogger(&GormLogger{})
+	dbc.LogMode(true)
+	return dbc, nil
 }
 
 func AutoMigrate() error {
@@ -27,4 +33,15 @@ func AutoMigrate() error {
 	db.AutoMigrate(&model.Member{}, &model.Task{}, &model.Iteration{})
 	log.Info("Migrate database done.")
 	return nil
+}
+
+type GormLogger struct{}
+
+func (*GormLogger) Print(v ...interface{}) {
+	if v[0] == "sql" {
+		log.WithFields(log.Fields{"module": "gorm", "type": "sql"}).Print(v[3])
+	}
+	if v[0] == "log" {
+		log.WithFields(log.Fields{"module": "gorm", "type": "log"}).Print(v[2])
+	}
 }
