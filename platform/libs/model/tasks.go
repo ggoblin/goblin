264a5102428ecd4c34cb9e98089d5a08e8ba9225
eternnoir <eternnoir@gmail.com>
2016-05-01 16:05:21 +0800

    Update task's date issue.

--- platform/libs/model/tasks.go
+++ platform/libs/model/tasks.go
@@ -11,11 +11,16 @@ type Task struct {
 	IterationId *string    `gorm:"not null"`
 	Name        *string    `gorm:"not null"`
 	Point       *float64   `gorm:"not null"`
-	StartDate   JsonTime   `gorm:"-" json:"-"`
-	EndDate     JsonTime   `gorm:"-" json:"-"`
-	StartTime   *time.Time `gorm:"not null"`
-	EndTime     *time.Time `gorm:"not null"`
+	StartDate   *JsonTime  `gorm:"-"`
+	EndDate     *JsonTime  `gorm:"-"`
+	StartTime   *time.Time `gorm:"not null" json:"-"`
+	EndTime     *time.Time `gorm:"not null" json:"-"`
 	Raw         *string    `sql:"json"`
+}
+
+func (it *Task) SetDate() {
+	it.StartDate = &JsonTime{it.StartTime}
+	it.EndDate = &JsonTime{it.EndTime}
 }
 
 func (it *Task) SetTime() {
